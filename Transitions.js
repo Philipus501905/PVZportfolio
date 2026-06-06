1df6457d7e41e36142ea9053d2c5a0821fd4c0f0
Philipus501905 <501905@vistacollege.nl>
2026-06-06 23:55:02 +0200

    cleanup js

--- Transitions.js
+++ Transitions.js
@@ -8,20 +8,6 @@ document.querySelectorAll('a').forEach(function(link) {
     }
     if (href.includes('bewijs')) {
       document.documentElement.dataset.direction = 'up';
-    }
-  });
-});
-
-document.querySelectorAll('a').forEach(function(link) {
-  link.addEventListener('click', function(e) {
-    const href = this.getAttribute('href');
-    const id = this.getAttribute('id');
-    if (!href || href === '' || href.includes('.pdf') || href.includes('.docx') || id === 'contact-link' || id === 'furhat-link' || id === 'DI-Lab-link' || id === 'Watersley-link') {
-      e.preventDefault();
-      return;
-    }
-    if (href.includes('bewijs')) {
-      document.documentElement.dataset.direction = 'up';
     } else if (href.includes('index')) {
       document.documentElement.dataset.direction = 'down';
     }
